2ba5dc71d1a0293c103393601651fdd8c6eab983
Bob Farias <bobhenrique9@gmail.com>
2023-11-17 17:43:51 +0100

    [ADD] implementing the insertNodeTailLinkedList and TestinsertNodeTailLinkedList

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
+import os
+
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
 
 
 class LinkedList:
@@ -47,13 +60,31 @@ def printElementsLinkedList(head):
     print(head.data)
     printElementsLinkedList(head.next)
 
-    # Iterative method
-    if head is None:
-        return head
+    # # Iterative method
+    # if head is None:
+    #     return head
 
-    while head:
-        print(head.data)
-        head = head.next
+    # while head:
+    #     print(head.data)
+    #     head = head.next
+
+
+def insertNodeTailLinkedList(head, data):
+    if head is None:
+        new_node = Node(data or 0)
+        return new_node
+
+    # Iterative mode
+    #  current_node here is necessary:
+    #  to preserve the reference to the original head of the linked list
+    #  after the loop, head is still pointing to the first node.
+    current_node = head
+
+    while current_node.next:
+        current_node = current_node.next
+
+    current_node.next = Node(data)
+    return head
 
 
 # First Usage of the implementation:
@@ -65,4 +96,5 @@ if __name__ == "__main__":
     linked_list.append(3)
     linked_list.append(4)
 
-    printElementsLinkedList(linked_list.head)
+    # printElementsLinkedList(linked_list.head)
+    insertNodeTailLinkedList(linked_list.head, 200)
--- test_cases.py
+++ test_cases.py
@@ -1,7 +1,7 @@
 import unittest
 import io
 from unittest.mock import patch
-from index import Node, LinkedList, printElementsLinkedList
+from index import Node, LinkedList, printElementsLinkedList, insertNodeTailLinkedList
 
 
 class TestPrintElementsLinkedList(unittest.TestCase):
@@ -26,6 +26,49 @@ class TestPrintElementsLinkedList(unittest.TestCase):
         expected_output = '\n'.join(map(str, elements))
 
         self.assertEqual(printed_output, expected_output)
+
+
+class TestinsertNodeTailLinkedList(unittest.TestCase):
+    def test_empty_list_empty_data(self):
+        linked_list = LinkedList()
+        # Linked List None and Data empty
+        result_head = insertNodeTailLinkedList(None, None)
+        self.assertIsNotNone(
+            result_head, "Need to be equal to 0 because it will create a default Node as 0")
+        self.assertEqual(
+            result_head.data, 0, "Need to be equal to 0 because it will create a default Node as 0")
+
+        # Linked List None and Data true
+        result_head = insertNodeTailLinkedList(None, 200)
+        self.assertIsNotNone(
+            result_head, "Need to be equal to 0 because it will create a default Node as 0")
+        self.assertEqual(result_head.data, 200,
+                         "Need to be equal to 0 because it will create a default Node as 0")
+
+        # Linked List true and Data None
+        result_head = insertNodeTailLinkedList(linked_list.head, None)
+        self.assertIsNotNone(
+            result_head, "Need to be equal to 0 because it will create a default Node as 0")
+        self.assertEqual(
+            result_head.data, 0, "Need to be equal to 0 because it will create a default Node as 0")
+
+    def test_non_empty_list_with_data(self):
+        # Test case for a non-empty linked list and some data
+        provide_data = 200
+        elements = [1, 2, 3, 4, 5, 6, 7, 5, 4,
+                    3, 2, 1, 2, 4, 5, 6, 7, 8, 6, 5, 4]
+        linked_list = create_linked_list(elements)
+        result_head = insertNodeTailLinkedList(linked_list, provide_data)
+        self.assertIsNotNone(
+            result_head, "Should not be None because we have linked list and data")
+
+        current_node = result_head
+        while current_node.next:
+            current_node = current_node.next
+        self.assertEqual(current_node.data, provide_data,
+                         "New node at the tail should have the provided data.")
+        self.assertIsNone(current_node.next,
+                          "Next node after the new node should be None.")
 
 
 def create_linked_list(elements):
